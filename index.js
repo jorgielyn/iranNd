d578b87464c5cb1c13deb308867662f0206d74cd
jorgielyn <54056647+jorgielyn@users.noreply.github.com>
2019-09-03 13:09:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var student = require("./student")
 var input = require("readline-sync")
 var list=  []
+var student = require("./student")
+var input = require("readline-sync")
+//var toArray=  require("to-array")
+const deepcopy = require('deepcopy')
 var http = require("http")
-
+var list = []
 var jorge = new student();
 //the program runs until "YES" is inputed!
+do{
 do {
     console.log("Add Student : ")
     jorge.setName(input.question("\tName : "))
@@ -12,18 +17,22 @@ do {
     jorge.addCourse(input.question("\tCourse : "),input.question("\tYear : "))
     console.log("Add Schedule :")
     jorge.addSchedule(input.question("\tDay : "),input.question("\tTime : "))
-            //arrays
+} while (input.question("exit? : ")!= "yes");
+    console.log("Successfully Added data!\n")
+    //to view the data
+    if(input.question("Wanna view the Data Added? ")!="no\n"){
+    //arrays
     list.push(jorge)
     console.log(list)
-} while (input.question("exit? : ")!= "yes");
-    console.log("Successfully Added data!")
-
-    //arrays
-// list.push(jorge)
-// console.log(list)
+    // list.push(jorge);
+    // list.toArray();
+    }else("Thank You! \t Come again!")
+} while(input.question("exit? : ")!="yes");
+    console.log("Thank You!")
     //creating a server
 http.createServer(function(request , response){
     console.log("server started!")
     response.write()//start the response
     response.end()//end the sever response
-}).listen(8080)
+}).listen(8000)
+
