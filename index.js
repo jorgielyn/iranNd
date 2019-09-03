94fd7f6c9f9f967ab14c651cbf98486d1a2230f0
jorgielyn <54056647+jorgielyn@users.noreply.github.com>
2019-09-03 11:10:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var input = require("readline-sync")
 var http = require("http")
 
 var jorge = new student();
+//the program runs until "YES" is inputed!
 do {
     console.log("Add Student : ")
     jorge.setName(input.question("\tName : "))
@@ -12,9 +13,9 @@ do {
     jorge.addSchedule(input.question("\tDay : "),input.question("\tTime : "))
 } while (input.question("exit? : ")!= "yes");
     console.log("Successfully Added data!")
+    //creating a server
 http.createServer(function(request , response){
     console.log("server started!")
-    response.write()
-    response.end()
-}).listen(8888)
-
+    response.write()//start the response
+    response.end()//end the sever response
+}).listen(8080)
